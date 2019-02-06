3e6297315390fc55a7ffc0e201b1b88a768caefb
grosjeanstephanie <grosjean.stephaniep@gmail.com>
2019-02-06 09:22:35 +0100

    news

---
+++ script2.js
@@ -0,0 +1,7 @@
+document.querySelector(".title").addEventListener("mouseover",()=>{
+  document.querySelector("#rain").style.background="linear-gradient(#d5adc8, #ff8489) fixed";
+
+})
+document.querySelector(".title").addEventListener("mouseout",()=>{
+  document.querySelector("#rain").style.background="linear-gradient(red,black) fixed";
+})
